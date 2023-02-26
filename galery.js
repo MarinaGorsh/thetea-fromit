ca35cd8a0d76146b19934406ef9938dbedfb5aee
MarinaGorsh <69041379+MarinaGorsh@users.noreply.github.com>
2023-02-26 16:17:03 +0100

    Add files via upload

---
+++ galery.js
@@ -0,0 +1,15 @@
+function openImg(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    expandImg.src = imgs.src;
+    expandImg.parentElement.style.display = "block";
+}
+function openImg1(imgs) {
+    var expandImg1 = document.getElementById("expandedImg1");
+    expandImg1.src = imgs.src;
+    expandImg1.parentElement.style.display = "block";
+}
+function openImg2(imgs) {
+    var expandImg2 = document.getElementById("expandedImg2");
+    expandImg2.src = imgs.src;
+    expandImg2.parentElement.style.display = "block";
+}
